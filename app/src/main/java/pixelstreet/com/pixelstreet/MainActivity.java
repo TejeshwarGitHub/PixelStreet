aa9e865aeee5a650a9d2fed6200778ab078f98b3
Tejeshwar Reddy Gurram <tejeshwarreddy1996@gmail.com>
2016-02-10 22:56:05 +0530

    Starting FB

--- app/src/main/java/pixelstreet/com/pixelstreet/MainActivity.java
+++ app/src/main/java/pixelstreet/com/pixelstreet/MainActivity.java
@@ -1,30 +1,173 @@
 package pixelstreet.com.pixelstreet;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Base64;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.viewpagerindicator.CirclePageIndicator;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import pixelstreet.com.pixelstreet.helper.FacebookButtonTransformer;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageView background;
+    ViewPager mViewPager;
+    TextView skip;
+    View loginWrapper;
+
+    LoginFragment loginFragment;
+    CallbackManager mCallbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FacebookSdk.sdkInitialize(getApplicationContext());
 
-        printHashKey();
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        checkLoggedIn();
+        setContentView(R.layout.activity_main);
+        mCallbackManager = CallbackManager.Factory.create();
+        background = (ImageView) findViewById(R.id.loginBackground);
+        mViewPager = (ViewPager) findViewById(R.id.loginTextContainer);
+        skip = (TextView) findViewById(R.id.skipLogin);
+        loginWrapper = findViewById(R.id.loginWrapper);
+        CirclePageIndicator circlePageIndicator = (CirclePageIndicator) findViewById(R.id.circle_indicators);
+
+        final SectionPagerAdapter mPagerAdapter = new SectionPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
+        circlePageIndicator.setViewPager(mViewPager);
+
+        LoginManager.getInstance().registerCallback(mCallbackManager,
+                new FacebookCallback<LoginResult>() {
+                    private ProfileTracker mProfileTracker;
+
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.e("LoginFacebook", "Success");
+                        mProfileTracker = new ProfileTracker() {
+                            @Override
+                            protected void onCurrentProfileChanged(Profile profile, Profile profile1) {
+                                Log.e("facebook - profile", profile1.getFirstName());
+                                mProfileTracker.stopTracking();
+                            }
+                        };
+                        mProfileTracker.startTracking();
+                        logIn();
+
+
+                        // TODO: 21-10-2015  send user data to server,
+                        // TODO: 21-10-2015 add user data to table
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(MainActivity.this, "Login Cancel", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(MainActivity.this, exception.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        Button btn_fb_login = (Button) findViewById(R.id.login_button);
+
+        btn_fb_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logInWithReadPermissions(MainActivity.this, Arrays.asList("public_profile", "user_friends"));
+            }
+        });
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pos = mViewPager.getCurrentItem();
+                if (pos < mPagerAdapter.getCount() - 1) {
+                    mViewPager.setCurrentItem(mPagerAdapter.getCount() - 1);
+                } else {
+                    checkLoggedIn();
+                }
+            }
+        });
+        mViewPager.setPageTransformer(true, new FacebookButtonTransformer(this, loginWrapper, circlePageIndicator));
 
     }
-    void printHashKey()
+
+    private void logIn() {
+        checkLoggedIn();
+    }
+
+    private void checkLoggedIn() {
+
+        if (AccessToken.getCurrentAccessToken()!=null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onBackPressed() {
+       /* if(mViewPager.getCurrentItem()!=0){
+            mViewPager.setCurrentItem(0);
+        }
+        else*/
+        super.onBackPressed();
+    }
+
+    class SectionPagerAdapter extends FragmentStatePagerAdapter {
+
+        public SectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            Log.d("get Item", position + " ");
+            Fragment fragment = new LoginFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("index", position);
+            fragment.setArguments(bundle);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+}
+ /*void printHashKey()
     {
         try {
             PackageInfo info = getPackageManager().getPackageInfo(
@@ -40,5 +183,4 @@ public class MainActivity extends AppCompatActivity {
         } catch (NoSuchAlgorithmException e) {
 
         }
-    }
-}
+    }*/
---
+++ app/src/main/java/pixelstreet/com/pixelstreet/helper/FacebookButtonTransformer.java
@@ -0,0 +1,66 @@
+package pixelstreet.com.pixelstreet.helper;
+
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.TextView;
+
+import pixelstreet.com.pixelstreet.R;
+
+/**
+ * Created by Tejeshwar Reddy on 10-02-2016.
+ */
+public class FacebookButtonTransformer implements ViewPager.PageTransformer  {
+
+    Context context;
+    View loginWrapper;
+    View pageIndicator;
+
+    public FacebookButtonTransformer(Context context, View loginWrapper, View pageIndicator) {
+        this.context = context;
+        this.loginWrapper=loginWrapper;
+        this.pageIndicator=pageIndicator;
+    }
+
+    @Override
+    public void transformPage(View view, float position) {
+
+        int pageWidth = view.getWidth();
+        int pageHeight = view.getHeight();
+
+        if(((Integer)view.getTag())==3){
+
+            if(position<0){
+                view.setAlpha((position+1)/2);
+                view.setTranslationX(-position * pageWidth);
+                pageIndicator.setAlpha(0);
+            }
+            else {
+                view.setAlpha(1);
+                pageIndicator.setAlpha(1);
+            }
+
+        }
+        if(((Integer)view.getTag())==4) {
+
+            if(position<1&& position>=0) {
+                loginWrapper.setTranslationY(-(1 - position) * (pageHeight / 2));
+                SpannableString content = new SpannableString("Maybe later");
+                content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+                ((TextView)loginWrapper.findViewById(R.id.skipLogin)).setText(content);
+            }
+            else if(position>=0.8){
+                SpannableString content = new SpannableString("skip intro");
+                content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+                ((TextView)loginWrapper.findViewById(R.id.skipLogin)).setText(content);
+            }
+
+        }
+
+
+
+    }
+}
