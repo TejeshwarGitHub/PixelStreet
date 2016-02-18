c089d05f4a62e46632393084a4735490fb9f92f1
Tejeshwar Reddy Gurram <tejeshwarreddy1996@gmail.com>
2016-02-18 23:50:00 +0530

    3rd commit

---
+++ app/src/main/java/pixelstreet/com/pixelstreet/ProfileDetailsActivity.java
@@ -0,0 +1,27 @@
+package pixelstreet.com.pixelstreet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class ProfileDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.hire_now);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
